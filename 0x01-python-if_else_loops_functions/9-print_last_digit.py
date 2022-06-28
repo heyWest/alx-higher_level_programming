601b22eb1842f4e1b6110da420430c64f27c2aab
Daniel Manasseh Ofei Kwafo <heyitzwest@gmail.com>
2022-06-28 14:49:53 -0700

    task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bint/python3
 def print_last_digit(number):
-    print("{:d}".format(number % 10), end='')
+    if number >= 0:
+        print("{:d}".format(number % 10), end='')
+        return number % 10
+    else:
+        print("{:d}".format((number % -10) * -1), end='')
+        return (number % -10) * -1
