80bd2b1609f5615f14485dec8f7e4b8c0407ab15
Daniel Manasseh Ofei Kwafo <heyitzwest@gmail.com>
2022-06-30 09:11:25 -0700

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,13 +3,15 @@ if __name == "__main__":
     import sys
     var = len(sys.argv) - 1
 
-    if var == 1:
+    if var == 0:
+        print("{} arguments:".format(var))
+    elif var == 1:
         print("{} argument:".format(var))
     else:
         print("{} arguments:".format(var))
     if var >= 1:
         var = 0
-        for arg in  sys.argv:
+        for arg in sys.argv:
             if var != 0:
                 print("{}: {}".format(var, arg))
             var += 1
