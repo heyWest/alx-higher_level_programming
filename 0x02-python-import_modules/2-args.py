8347d23a118d67b6a60404ba9fc889137c23990e
Daniel Manasseh Ofei Kwafo <heyitzwest@gmail.com>
2022-06-30 09:06:52 -0700

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name == "__main__":
+    import sys
+    var = len(sys.argv) - 1
+
+    if var == 1:
+        print("{} argument:".format(var))
+    else:
+        print("{} arguments:".format(var))
+    if var >= 1:
+        var = 0
+        for arg in  sys.argv:
+            if var != 0:
+                print("{}: {}".format(var, arg))
+            var += 1
