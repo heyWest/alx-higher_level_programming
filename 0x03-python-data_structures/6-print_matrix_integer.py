7f5a941d5349cada96029de8e64c4b7b4ca699f9
Daniel Manasseh Ofei Kwafo <79490695+heyWest@users.noreply.github.com>
2022-07-04 10:02:20 +0000

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
+            if j != 0:
+                print(" ", end='')
             print("{:d}".format(j), end= ' ')
         print()
