e37be4806a3b6ed99d09a44fa5ca8b5cede3d736
Daniel Manasseh Ofei Kwafo <79490695+heyWest@users.noreply.github.com>
2022-07-06 17:22:54 +0000

    Update 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for key in a_dictionary:
-        new_dict[key] = a_dictionary[key] * 2
+    new_dict = a_dictionary.copy()
+    for key in list(new_dict.keys()):
+        new_dict[key] *= 2
     return new_dict
