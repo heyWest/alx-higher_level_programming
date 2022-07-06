bc6c37b659d50cf309969a98fc49a87ae3486013
Daniel Manasseh Ofei Kwafo <79490695+heyWest@users.noreply.github.com>
2022-07-06 16:37:53 +0000

    Update 6-print_sorted_dictionary.py

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    new_dict = dict(sorted(a_dictionary.items(), key = lambda x:x[0]))
+    new_dict = dict(sorted(a_dictionary.items(), key = lambda x:x[0], reverse=True))
     for key, val in new_dict.items():
         print("{}: {}".format(key, val))
