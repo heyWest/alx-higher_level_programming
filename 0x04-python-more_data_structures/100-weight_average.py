df2824541f2e1c347b4e29534a96a4f799e4245a
Daniel Manasseh Ofei Kwafo <heyitzwest@gmail.com>
2022-07-06 11:03:22 -0700

    task 100

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list:
+        acc = 0
+        var = 0
+        for x in my_list:
+            acc += x[0] * x[1]
+            var += x[1]
+        return (acc / var)
