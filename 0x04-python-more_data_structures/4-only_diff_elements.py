9c014d219a4661f03d095c4a57dabdeefc0d6cbe
Daniel Manasseh Ofei Kwafo <79490695+heyWest@users.noreply.github.com>
2022-07-06 16:07:45 +0000

    Update 4-only_diff_elements.py

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    return list(filter(lambda x: x not in set_2, set_1))
+    if len(set_1) > len(set_2):
+        final = list(filter(lambda x: x not in set_2, set_1))
+    else:
+        final = list(filter(lambda x: x not in set_1, set_2))
+    return final
