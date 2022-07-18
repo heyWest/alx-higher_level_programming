76893a940e0386645b094c90ba493aeadefd4d26
Daniel Manasseh Ofei Kwafo <79490695+heyWest@users.noreply.github.com>
2022-07-18 15:29:23 +0000

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0
-
-    for i in range(x):
-        try:
-            print("{}".format(my_list[i]), end='')
-        except:
-            break
-        else:
-            count += 1
-
-    print()
-    return (count)
+    counter = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end='')
+            counter += 1
+        print()
+        return counter
+    except:
+        print()
+        return counter
